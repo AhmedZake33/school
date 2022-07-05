4c8ee7617e74fd887f419ca152d9bf8dc2d15e3a
ahmed zake <ahmed.zake333@gmail.com>
2022-07-05 20:20:18 +0200

    update

--- src/components/addData.vue
+++ src/components/addData.vue
@@ -1,6 +1,8 @@
 <template>
     <v-app>
-        aadd data
+       <div class="addForm">
+        <h2>إضافه طفل جديد</h2>
+        </div>
     </v-app>
 </template>
 
@@ -13,5 +15,15 @@ export default {
 
 
 <style scoped>
+.addForm {
+     padding-top:50px ;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 
+.addForm h2{
+    font-weight: bold;
+    margin: right;
+}
 </style>
